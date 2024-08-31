bc469f7f5d5f0e6549d40b6f9ece628b4eadeca5
franck gaspoz <franck.gaspoz@gmail.com>
2024-08-31 03:46:38 +0200

    fix launcher v2

--- imdb_scraper/launcher.py
+++ imdb_scraper/launcher.py
@@ -1,6 +1,5 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from imdb_scraper.imdb_scraper.spiders.movie import MovieSpider
 
 settings = get_project_settings()
 

ef7e60c531a9c4d3659fcfe4675877ea2645817f
franck gaspoz <franck.gaspoz@gmail.com>
2024-08-31 02:57:03 +0200

    cleanup

--- imdb_scraper/imdb_scraper/launcher.py
+++ imdb_scraper/imdb_scraper/launcher.py
@@ -1,9 +1,5 @@
-import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-import imdb_scraper
-
-# useful if you have settings.py
 from imdb_scraper.imdb_scraper.spiders.movie import MovieSpider
 
 settings = get_project_settings()
